f6f2eb2dec5cf8b193666cfaef9bdf776781338b
Kevin Hood <nissmotion@gmail.com>
2023-05-24 10:05:57 -0400

    register observers

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,14 @@
 
 namespace App\Providers;
 
+use App\Models\Todo;
+use App\Models\NestedTodo;
+use App\Observers\TodoObserver;
+use App\Observers\NestedTodoObserver;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -25,7 +29,8 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        NestedTodo::observe(NestedTodoObserver::class);
+        Todo::observe(TodoObserver::class);
     }
 
     /**
